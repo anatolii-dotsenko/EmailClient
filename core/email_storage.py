bc042b7be70c8625ed5c0ddadef24fbc34d3171f
anatolii-dotsenko <a.dotsenko.dev@gmail.com>
2025-12-21 22:35:16 +0200

    from scratch

--- core/email_storage.py
+++
@@ -1,7 +0,0 @@
-import json
-
-class EmailStorage:
-    def save_to_json(self, emails, path="data/emails.json"):
-        # save emails to json file
-        with open(path, "w", encoding="utf-8") as file:
-            json.dump(emails, file, indent=4, ensure_ascii=False)
